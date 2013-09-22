d2ef15021b0a68ba0a59c3fc2d56fc632cc4f6b1
Pavel Kirienko <pavel.kirienko@gmail.com>
2013-09-22 19:52:23 +0400

    Fixed canas_control.py

--- tools/canas_control.py
+++ tools/canas_control.py
@@ -4,7 +4,7 @@ import sys, pycanbus, struct, random
 
 SELF_NODE_ID             = 255
 TX_DATA_TYPE             = 9
-MSGID_GRIPPER_GRIP_CMD   = 450
+CANAS_UAV_GRIPPER_GRIP_COMMAND = 1820
 
 if len(sys.argv) < 2:
     print 'Usage:   %s <iface> [0|1]' % sys.argv[0]
@@ -15,9 +15,15 @@ sock = pycanbus.Socket(sys.argv[1])
 
 if len(sys.argv) > 2:
     on = int(sys.argv[2])
-    msgcode = int(random.random() * 255)
+    try:
+        msgcode = int(open('/tmp/opengrab_canas_control_msgcode').read())
+    except IOError, ValueError:
+        msgcode = 0
+    msgcode += 1
+    with open('/tmp/opengrab_canas_control_msgcode', 'w') as f:
+        f.write(str(msgcode))
     data = struct.pack('>BBBBB', SELF_NODE_ID, TX_DATA_TYPE, 0, msgcode, on)
-    frame = pycanbus.Frame(data, MSGID_GRIPPER_GRIP_CMD, False)
+    frame = pycanbus.Frame(data, CANAS_UAV_GRIPPER_GRIP_COMMAND, False)
     sock.send(frame)
 else:
     while 1:
